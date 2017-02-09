503ea422cd4836bbf62bf3a44027471e22075454
manthanbhalla <manthan.bhalla4@gmail.com>
2017-02-09 16:00:30 -0500

    contact page added

--- generator/routes/index.js
+++ generator/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/Home', function(req, res, next) {
+    res.render('Home', { title: 'Home' });
+});
+
+router.get('/About', function(req, res, next) {
+    res.render('About', { title: 'About' });
+});
+
+router.get('/Project', function(req, res, next) {
+    res.render('Prohject', { title: 'Project' });
+});
+
+router.get('/Service', function(req, res, next) {
+    res.render('Service', { title: 'Service' });
+});
+
+router.get('/Contact', function(req, res, next) {
+    res.render('Contact', { title: 'Contact' });
+});
 module.exports = router;
